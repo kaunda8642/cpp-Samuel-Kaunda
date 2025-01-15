34af74d3e0ba988ebbfad3802fab9d574c40615f
kaunda8642 <90796843+kaunda8642@users.noreply.github.com>
2025-01-15 13:44:08 -0600

    Add files via upload
    Happy birthday in class assignment.

---
+++ C++ new language/HappyBirthday.cpp
@@ -0,0 +1,21 @@
+// HappyBirthday.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <String>
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?\n";
+    std::cin >> name; 
+
+    std::cout << "Happy Birthday to you!\n";
+    std::cout << "Happy Birthday to you!\n";
+    std::cout << "Happy Birthday dear !";
+    std::cout << name;
+    std::cout << "\n"; 
+    std::cout << "Happy Birthday to you!\n";
+    return 0; 
+}
+
+
